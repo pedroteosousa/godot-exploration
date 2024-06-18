20c3f3e59d9561286442f233cd182afac78bedd4
Pedro Sousa <pedroteosousa@gmail.com>
2024-06-18 01:30:08 -0300

    Fix infinite dashes

--- elements/player/fsm/dash.gd
+++ elements/player/fsm/dash.gd
@@ -1,8 +1,8 @@
 @tool
 extends FSMTransition
 
-func _on_transition(_delta, _actor, _blackboard: Blackboard):
-	pass
+func _on_transition(_delta, player, _blackboard: Blackboard):
+	player.can_dash = false
 
 func is_valid(player, _blackboard: Blackboard):
-	return player.should_dash()
+	return player.should_dash() and player.can_dash and player.has_dash
--- elements/player/fsm/on_ground.gd
+++ elements/player/fsm/on_ground.gd
@@ -3,6 +3,7 @@ extends FSMState
 
 func _on_enter(player, _blackboard: Blackboard):
 	player.can_double_jump = true
+	player.can_dash = true
 
 func _on_update(_delta, player, _blackboard: Blackboard):
 	player.update_horizontal_movement()
